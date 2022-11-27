89cbc848b1d9c9e024078c09b7a876fe7a758452
Bruno Augusto <brunaugusto@gmail.com>
2022-11-27 07:30:09 -0300

    2-Motores

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,54 @@ import styles from './App.module.css'
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/claro-bruno.png',
+      name: 'Bruno Augusto',
+      role: 'Backend Developer'
+    },
+    content: [
+      { 
+        type: 'paragraph', 
+        content: "Fala galeraa 👋" 
+      },
+      {
+        type: 'paragraph',
+        content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare'
+      },      
+    ],
+    publishedAt: new Date('2022-11-10 20:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernando',
+      role: 'CTO Rocketseat'
+    },
+    content: [
+      { 
+        type: 'paragraph', 
+        content: "Fala galeraa 👋" 
+      },
+      {
+        type: 'paragraph',
+        content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare'
+      },      
+    ],
+    publishedAt: new Date('2022-10-20 20:00')
+  },
+];
 export function App() {
 
   return (
@@ -14,14 +62,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Bruno Augusto" 
-            conteudo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ratione corrupti, quia asperiores fuga incidunt veritatis nobis similique ad quas quaerat nesciunt fugiat aliquid inventore corporis esse dolorum a sapiente!" 
-          />
-          <Post 
-            author="Gabriel Augusto" 
-            conteudo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ratione corrupti, quia asperiores fuga incidunt veritatis nobis similique ad quas quaerat nesciunt fugiat aliquid inventore corporis esse dolorum a sapiente!" 
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+                
+              />
+            )
+          })}
         </main>
       </div>
       
